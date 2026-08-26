1ebcdb5fef0d17fc8ee6cf56bb14295db4508924
KaahRomao <kaueromao1989@gmail.com>
2026-08-25 21:34:56 -0300

    parte do header

--- projeto-tailwind/src/componentes/Header.jsx
+++ projeto-tailwind/src/componentes/Header.jsx
@@ -1,15 +1,31 @@
-import Logo from "../assets/terno-do-noivo.png";
+import Logo from "../assets/minhaLogo-removebg-preview.png";
+import Lupa from "../assets/003-lupa.png";
+import User from "../assets/002-do-utilizador.png";
+import Carrinho from "../assets/001-vista-lateral-vazia-do-carrinho-de-compras.png";
 
 function Header() {
   return (
-    <div className="w-full h-25 bg-[#216869] flex items-center justify-between">
-      <img src={Logo} alt="" className="ml-6 w-14 h-14 max-w-full block" />
-      <nav className="flex items-center justify-between gap-6 mr-6">
-        <li className="p-16 bg-[#1F2421] rounded-lg text-[#49A078]">Home</li>
-        <li>produtos</li>
-        <li>sobre</li>
-        <li>Contato</li>
+    <div className="w-full h-20 bg-[#0A0F0D] flex items-center justify-between gap-5">
+      <img src={Logo} alt="" className="w-22 h-auto m-2.5 ml-14" />
+      <nav className="flex items-center justify-between gap-6">
+        <li className="p-1.5 bg-[#1F2421] rounded-lg text-white hover:scale-105 hover:text-purple-600">
+          Home
+        </li>
+        <li className="p-1.5 bg-[#1F2421] rounded-lg text-white hover:scale-105 hover:text-purple-600">
+          produtos
+        </li>
+        <li className="p-1.5 bg-[#1F2421] rounded-lg text-white hover:scale-105 hover:text-purple-600">
+          sobre
+        </li>
+        <li className="p-1.5 bg-[#1F2421] rounded-lg text-white hover:scale-105 hover:text-purple-600">
+          Contato
+        </li>
       </nav>
+      <div className="flex items-center justify-between gap-10 mr-14">
+        <img src={Lupa} alt="a" />
+        <img src={User} alt="a" />
+        <img src={Carrinho} alt="a" />
+      </div>
     </div>
   );
 }
